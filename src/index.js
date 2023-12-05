83467a41e71c0580fcc8fb885a211dbe5841cbef
Royal <royalhuseyn902@gmail.com>
2023-12-05 18:53:55 +0400

    Add event listeners to nav links

--- src/index.js
+++ src/index.js
@@ -5,4 +5,7 @@ import loadContactPage from "./contact-page";
 
 (() => {
   loadHomePage();
+
+  const navLinks = document.querySelectorAll("navbar > li");
+  navLinks.forEach(link => link.addEventListener("click", loadPage));
 })();
