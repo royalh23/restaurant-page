16a83992a15c28948120a2b50a35d8cb969d48c9
Royal <royalhuseyn902@gmail.com>
2024-01-17 22:53:49 +0400

    Remove IIFE from index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import "./style.css";
 import loadPage from "./load-page";
 import loadWebsite from "./website";
 
-(() => {
-  loadWebsite();
+loadWebsite();
 
-  const navLinks = document.querySelectorAll(".navbar li");
-  navLinks.forEach(link => link.addEventListener("click", loadPage));
-})();
+const navLinks = document.querySelectorAll(".navbar li");
+navLinks.forEach(link => link.addEventListener("click", loadPage));
